2af85fecc8754d0c0016a8401af0b01b539d4d2f
italolins <italo.lins@ccc.ufcg.edu.br>
2014-09-09 17:18:34 -0300

    rodando rodando

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import models.Viagem;
+import models.dao.GenericDAO;
+import models.dao.GenericDAOImpl;
 import play.*;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
+	private static GenericDAO dao = new GenericDAOImpl();
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
@@ -15,4 +20,23 @@ public class Application extends Controller {
     	return ok(cadastroViagens.render("Your new application is ready."));
     }
 
+    public static Result novaViagem(){
+    	DynamicForm requestData = Form.form().bindFromRequest();
+    	
+    	String local = requestData.get("local");
+    	String data = requestData.get("data");
+    	String descricao = requestData.get("descricao");
+    	
+    	Viagem v = new Viagem(local,data,descricao);
+    	
+    	return ok(index.render("=p"));
+    }
+    
+    public static GenericDAO getDao() {
+		return dao;
+	}
+
+	public static void setDao(GenericDAO dao) {
+		Application.dao = dao;
+	}
 }
