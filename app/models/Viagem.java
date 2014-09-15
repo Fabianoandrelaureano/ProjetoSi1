6d663a42f02e675de24f6a6bb5af389e9515df3f
italolins <italo.lins@ccc.ufcg.edu.br>
2014-09-14 22:41:32 -0300

    problema resolvido

--- app/models/Viagem.java
+++ app/models/Viagem.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -32,7 +33,7 @@ public class Viagem {
 //	@Column
 	private String descricao;
 	
-	@OneToMany(cascade=CascadeType.ALL)
+	@ManyToMany(cascade=CascadeType.ALL)
 	@JoinColumn  
 	private List<Usuario> pessoasNaViagem;
 	
